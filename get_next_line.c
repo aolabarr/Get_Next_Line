d018d978af997c3028363a252f11af15dd7b29ed
Aritz Olabarriaga Santamaria <aolabarr@c4r4s2.42urduliz.com>
2024-03-23 16:53:33 +0100

    subida 2

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: aolabarr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/23 11:58:55 by aolabarr          #+#    #+#             */
-/*   Updated: 2024/03/23 14:16:29 by aolabarr         ###   ########.fr       */
+/*   Updated: 2024/03/23 16:51:33 by aolabarr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,24 @@
 char	*get_next_line(int fd)
 {
 	static char *buffer;
-	static char	*bufplus;
 	static int	start;
-	static int	finbuf;
+	int			num;
 	char		*line;
 
-	if(!buffer)
+	line = NULL;
+if(!buffer)
 	{
-		ft_read_next_buffer(fd, buffer);
+		num = ft_read_next_buffer(fd, buffer);
 		start = 0;
-		finbuf = 0;i
-
-	leer en bucle hasta encontrar el \n o \0
 	}
-	if ft_linelen(buffer + start)
+	while (!ft_linelen(buffer) &&  num == BUFFER_SIZE)
 	{
-		line = ft_manage_line(bufplus, buffer, start, finbuf);
+		line = bufferjoin(line, buffer, num);
+		num = ft_read_next_buffer(fd, buffer);
+	}
+	if num == BUFFER_SIZE
+	{
+		line = ft_manage_line(buffer, start, finbuf);
 		start = start + ft_linelen(line);
 		return(line);
 	}
@@ -40,70 +42,43 @@ char	*get_next_line(int fd)
 		start = start + ft_finalfilelen(line);
 		return(line);
 	}
+}
+
+size_t	ft_read_next_buffer(int fd, char *buffer)
+{
+	if !(buffer)
+		buffer = malloc((BUFFER_SIZE) * sizeof(char));
+	num = read(fd, buffer, sizeof(buffer));
+	return(num);
+}
+
+char	*ft_bufferjoin(char line, char *buffer, int num)
+{
+	char	*str;
+
+	if (line != NULL)
+	{
+		if (num == BUFFER_SIZE)
+		{
+			str = malloc((sizeof(line) + BUFFER_SIZE * sizeof(char));
+			ft_memcpy(str, line, sizeof(line));	
+			ft_memcpy(str + sizeof(line) + 1, buffer, sizeof(buffer));
+			free(line);
+		}
+		else	
+		{
+			str = malloc((sizeof(line) + num * sizeof(char));
+			ft_memcpy(str, line, sizeof(line));	
+			ft_memcpy(str + sizeof(line) + 1, buffer, num * sizeof(char));
+			free(line);
+		}
+	}
 	else
 	{
-		bufplus = ft_manage_end_buffer(surplus, buffer, start);
-		finbuf = 1;
-
+		if (num == BUFFER_SIZE)
+			ft_memcpy(str, buffer, sizeof(buffer));
+		else
+			ft_memcpy(str, buffer, num * sizeof(char));
 	}
-}
-
-void	ft_read_next_buffer(int fd, char *buffer)
-{
-	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
-	read(fd, buffer, sizeof(buffer));
-	buffer (BUFFER_SIZE + 1) = '\0';
-}
-
-char	*ft_manage_line(char *bufplus, char *buffer, start, finbuf) 
-{
-	char	*line;
-	if finbuf
-		line = ft_linejoin(bufplus, buffer, start);
-	else
-		line = ft_create_line(buffer, start);
-	return(line);
-}
-
-char	*ft_linejoin(char *bufplus, char *buffer, int start)
-{
-	len = ft_linelen(buffer + start);
-
-}
-
-void	*ft_memcpy(void *dst, const void *src, size_t n)
-{
-	size_t	i;
-	
-	i = 0;
-	if (src == NULL && dst == NULL)
-		return (dst);
-	while (i < n)
-	{
-		((char *)dst)[i] = ((const char *)src)[i];
-		i++;
-	}
-	return (dst);
-}
-
-size_t	*ft_linelen(char *str)
-{
-	size_t	count;
-
-	count = 0;
-	while (str[i] !='\n')
-	   count++;
-	count++;
-	return(count);	
-}
-
-size_t	ft_strlen(const char *str)
-{
-	size_t	count;
-
-	count = 0;
-	while (str[count] != '\0')
-		count++;
-	count++;
-	return (count);
+	return (str);
 }
